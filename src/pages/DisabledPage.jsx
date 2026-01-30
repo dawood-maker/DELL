1af1468a8aa306378488db283d66c2aa56092a38
dawood-maker <your_email@example.com>
2026-01-30 13:00:59 +0500

    chang thre coding\

--- src/pages/DisabledPage.jsx
+++ src/pages/DisabledPage.jsx
@@ -3,11 +3,9 @@ function DisabledPage() {
     <div className="p-8">
       <h1 className="text-4xl font-bold mb-4">Disabled Page</h1>
       <p className="text-lg mb-2">
-        This page is “disabled” but you can still show information or a message here.
+        welcome my disabled pageq
       </p>
-      <p className="text-lg">
-        For example: "This feature is under development. Stay tuned!"
-      </p>
+
     </div>
   );
 }
--- src/pages/LinkPage.jsx
+++ src/pages/LinkPage.jsx
@@ -3,10 +3,7 @@ function LinkPage() {
     <div className="p-8">
       <h1 className="text-4xl font-bold mb-4">Link Page</h1>
       <p className="text-lg mb-2">
-        This is the Link page. You can display lists, links, or any related content here.
-      </p>
-      <p className="text-lg">
-        Example: <a href="https://example.com" className="text-blue-400 underline">External Link</a>
+      welcome my link page
       </p>
     </div>
   );
